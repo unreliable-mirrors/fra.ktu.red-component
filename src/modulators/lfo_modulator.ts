2c8d082a8322b2c3445296bda641490118d86916
colorrin <mb.andres@gmail.com>
2026-04-21 20:48:05 +0200

    LFO Waveforms

--- src/modulators/lfo_modulator.ts
+++ src/modulators/lfo_modulator.ts
@@ -6,6 +6,7 @@ import type { ModulatorState } from "./imodulator.js";
 export type LfoModulatorState = ModulatorState & {
   hz: number;
   phase: number;
+  waveform: "sine" | "square" | "triangle" | "sawtooth";
 };
 
 export class LfoModulator extends BaseModulator {
@@ -18,6 +19,7 @@ export class LfoModulator extends BaseModulator {
       name: "lfo_" + getCount(sceneStateId),
       hz: 1,
       phase: 0,
+      waveform: "sine",
     };
   }
   computeValue(): number {
@@ -25,7 +27,25 @@ export class LfoModulator extends BaseModulator {
       return 0;
     }
     const time = DataStore.getInstance().getStore("elapsedTime") || 0;
-    return (
+
+    if (this._state.waveform === "triangle") {
+      const beatDuration = 1000 / this._state.hz;
+      const position =
+        ((time % beatDuration) / beatDuration + this._state.phase) % 1;
+      if (position < 0.5) {
+        return position * 2;
+      } else {
+        return (1 - position) * 2;
+      }
+    } else if (this._state.waveform === "sawtooth") {
+      return (
+        ((time % (1000 / this._state.hz)) / (1000 / this._state.hz) +
+          this._state.phase) %
+        1
+      );
+    }
+
+    let v =
       Math.sin(
         ((time % (1000 / this.getFieldValue("hz"))) /
           (1000 / this.getFieldValue("hz")) +
@@ -34,7 +54,12 @@ export class LfoModulator extends BaseModulator {
           2,
       ) /
         2 +
-      0.5
-    );
+      0.5;
+    if (this.getFieldValue("waveform").toString() === "square") {
+      v = Math.round(v);
+    } else if (this.getFieldValue("waveform").toString() === "triangle") {
+      v = 1 - Math.abs(2 * v - 1);
+    }
+    return v;
   }
 }
